b7545dd98c8e2c9870c0b4fe04f1ad6ee7a379df
biz <bizplus0520@gmail.com>
2021-07-02 16:58:27 +0900

    블로그2 업로드

---
+++ src/main/java/Blog2/springBlog2/SpringBlog2Application.java
@@ -0,0 +1,15 @@
+package Blog2.springBlog2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+public class SpringBlog2Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringBlog2Application.class, args);
+	}
+
+}
